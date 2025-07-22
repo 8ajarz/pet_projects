2ae9a289a587d625a84e4f3aaf8f19a9c296dc2e
8ajarz <8ajarz_zaprasza@protonmail.com>
2025-07-22 21:03:51 +0700

    fixed bug with trimming first characters

--- divide_n_format_codes/divide_n_format_codes.py
+++ divide_n_format_codes/divide_n_format_codes.py
@@ -2,6 +2,7 @@
 
 import pandas as pd
 import re
+from time import time
 
 df = pd.read_excel('to_format.xlsx', sheet_name=0)
 
@@ -17,26 +18,29 @@ def row_format(df_updated):
     for index, row in df_updated.iterrows():
         if "??? " in row["Назва"]:
             row["Назва"] = row["Назва"][4:]
-        new = ""
-        old = re.findall(r'^\d{6,7} ', row["Назва"])
+        new = None
+        old = re.findall(r'^(\d*) ', row["Назва"])
         if old:
-            new = re.findall(r"\(.*(\d{6,7})\)", row["Назва"])
+            new = re.findall(r"\(\s*\D*(\d+)\D*\s*\)", row["Назва"][6:21])
             if new:
                 row["Old"] = old[0]
                 row["New"] = new[0]
-                row["Назва"] = row["Назва"][row["Назва"].index(new[0])  + len(new[0]) + 2:]
+                rest = row["Назва"].index(new[0]) + len(new[0]) + 1
+                row["Назва"] = row["Назва"][rest:]
             else:
                 row["Standard"] = old[0]
-                row["Назва"] = row["Назва"][row["Назва"].index(old[0]) + len(old[0]):]
+                row["Назва"] = row["Назва"][len(old[0]):]
         row["Назва"] = row["Назва"].strip().lstrip("- ")
         df_updated.loc[index] = row
 
     return df_updated
 
+start = time()
 df_to_save = row_format(df_updated)
+end = time()
 
 df_to_save.to_excel('output.xlsx', index=False)
-print("done")
+print("It took", end - start)
 
 if __name__ == "__main__":
     pass
